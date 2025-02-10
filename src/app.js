3761f2d28190595b617aa99bb9628b8ae4a09d0d
Divya1s2u3p4e5r6g7o <uppalapatidivya999@gmail.com>
2025-02-10 17:46:28 +0530

    Exploring get,post,delete methods

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 
 const app = express(); //Instance of express
 
-app.use("/hello",(req,res)=>{
-res.send("Hello,Hello,Hello!........");
-});
-
-app.use("/",()=>{
-    console.log("Namaste Divya!");
+//This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"John",lastName:"Doe"});
 })
+
+app.post("/user",(req,res)=>{
+    console.log("Save data To the database")
+    res.send("Data successfully save to the database!");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted Successfully!");
+})
+//This will match all the Http Method API calls to /test
 app.use("/test",(req,res)=>{
     res.send("Hello from the server");
 });
