7243a6278206461b49a4a51e970d6cc961f73391
Divya1s2u3p4e5r6g7o <uppalapatidivya999@gmail.com>
2025-03-13 15:39:00 +0530

    Divya::worked on signup,user,feed

--- src/app.js
+++ src/app.js
@@ -4,14 +4,11 @@ const connectDB = require('./config/database');
 const app = express(); //Instance of express
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup",async (req,res)=>{
     //Creating A new instance of the User model
-    const user = new User({
-        firstName:"Sravan",
-        lastName:"Kammari",
-        emailId:"sravan.k@sunkpo.com",
-        password:"Sravan@123"
-    })
+    const user = new User(req.body);
     try{
     await user.save();
     res.send("user added successfully");
@@ -19,6 +16,31 @@ app.post("/signup",async (req,res)=>{
         res.status(400).send("Error saving the user",+ err.message)
     }
 })
+//Get User By EmailId
+app.get("/user",async (req,res)=>{
+    try{
+        const emailId = req.body.emailId;
+        const users = await User.find({emailId:emailId});
+        if(users.length === 0){
+            res.send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+
+    }catch(err){
+        res.status(400).send("Error fetching users",+ err.message)
+    }
+})
+//Feed API Get /feed - get all the users from the database
+app.get("/feed",async (req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("Error fetching users",+ err.message)
+    }
+}) 
 connectDB().then(()=>{
     console.log("Database Connection established");
     app.listen(7777,()=>{
